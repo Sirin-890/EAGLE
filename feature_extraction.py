5296251c692dafc056cd3db48ce5617ef5e39b92
Sirin-890 <bappamorya639@gmail.com>
2025-05-12 12:59:54 +0530

    feature extarction

---
+++ feature_extraction.py
@@ -0,0 +1,12 @@
+import csv
+from prompts import*
+from predict_demo2 import*
+from loguru import logger
+
+folder_path = "CelebA_dataset_remaining"  # Path to the folder containing images
+output_csv = "new_features_CelebA_dataset_remaining.csv"
+input_prompt=prompt6
+facial_list= prompt6_feature_list
+
+if __name__=="__main__":
+    eagle_csv_generator(folder_path,output_csv,input_prompt,facial_list)
